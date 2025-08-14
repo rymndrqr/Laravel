6b90e88c45825dc518e4424b94aece51bec51292
Pong36 <fritzpasco9@gmail.com>
2025-08-14 15:48:05 +0800

    navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,23 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">    
     <title>Document</title>
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet"> 
+
+
 </head>
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
-    <nav class="navbar bg-primary">
-        <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
-    </nav>
+    <script src="public/js/bootstrap.bundle.min.js"></script>
+        <nav class="bg-muted bg-opacity-50 p-2 m-2">
+            <ol class="bg-dark">
+                <li class="text-warning">hahaha</li>
+                <li class="text-success">haahaha</li>
+                <li class="text-danger">hahaha</li>
+            </ol>
+        </nav>
 </body>
 </html>
 
